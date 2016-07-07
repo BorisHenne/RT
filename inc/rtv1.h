77d90c3ca87cb34f0e881f4baf19584e501e9619
Boris HENNE <bhenne@e1r11p14.42.fr>
2016-07-07 02:05:39 +0200

    .h + defince Esc

---
+++ inc/rtv1.h
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rtv1.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bhenne <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/07/07 01:48:17 by bhenne            #+#    #+#             */
+/*   Updated: 2016/07/07 02:03:23 by bhenne           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _RTV1_H
+# define _RTV1_H
+# include "libft.h"
+# include <math.h>
+# include <stdlib.h>
+# define ESCAPE	53
+
+typedef struct		 s_env
+{
+	void			*mlx;
+	void			*win;
+	void			*img;
+	char			*render;
+	int				projection;
+	int				rotation;
+	int				color_depth;
+	int				size_line;
+	int				endian;
+	char			*map_name;
+	int				fd;
+	int				rows;
+	int				columns;
+	float			span;
+	float			x;
+	float			y;
+	int				pan_x;
+	int				pan_y;
+	int				depth;
+	int				smooth;
+	t_read			*read_map;
+	t_pos			***map;
+	int				max;
+	int				min;
+	int				center_x;
+	int				center_y;
+	int				down;
+	int				up;
+	unsigned int	(*color)(struct s_env *);
+}					t_env;
+
+
+#endif
