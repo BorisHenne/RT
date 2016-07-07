945407f578f592cba8284d37f6f40fc0bc97a5ae
Boris HENNE <bhenne@e1r11p14.42.fr>
2016-07-08 01:28:22 +0200

    corrections camera

--- src/camera.c
+++ src/camera.c
@@ -6,31 +6,33 @@
 /*   By: sduprey <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/07 05:31:21 by sduprey           #+#    #+#             */
-/*   Updated: 2016/07/07 06:43:32 by sduprey          ###   ########.fr       */
+/*   Updated: 2016/07/08 01:28:00 by bhenne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <camera.h>
+#include "rtv1.h" 
 
-t_camera	*init_camera(t_vector *pos, t_vector *dir)
+t_cam	*init_camera(t_vec *pos, t_vec *dir)
 {
-	t_camera	*cam;
+	t_cam	*cam;
 
-	cam = (t_camera *)malloc(sizeof(t_camera));
+	cam = (t_cam*)malloc(sizeof(t_cam));
 	cam->camPos = pos;
 	cam->upVec = dir;
-	cam->viewplaneWidth = 1.0f;
-	cam->viewplaneHeight = 0.35f;
+	cam->viewplaneWidth = 0.35f;
+	cam->viewplaneHeight = 0.5f;
 	cam->viewplaneDist = 1.0f;
+	cam->viewPlaneUpLeft = camPos + ((vecDir*viewplaneDist)+(upVec*(viewplaneHeight/2.0f))) - (rightVec*(viewplaneWidth/2.0f))
 	return (cam);
 }
 
-t_vector	*calcDirVec(t_camera *cam, double x, double y, int xRes, int yRes)
+t_vec	*calcPos(t_cam *cam, int x, int y)
 {
 	double	xIndent;
 	double	yIndent;
 
-	xIndent = cam->viewplaneWidth / (double)xRes;
-	yIndent = cam->viewplaneHeight / (double)yRes;
-	return ((cam->viewPlaneUpLeft + cam->rightVec * xIndent * x - cam-upVec * yIndent * y) - cam->camPos);
+	xIndent = cam->viewplaneWidth / cam->xRes;
+	yIndent = cam->viewplaneHeight / cam->yRes;
+
+	return(cam->viewPlaneUpLeft + cam->rightVec*xIndent*x - cam->upVec*yIndent*y);
 }
