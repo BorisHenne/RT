6b062b172405e7079dc34ad4081ea4e04f1c0ae6
Boris HENNE <bhenne@e1r11p14.42.fr>
2016-07-08 03:00:05 +0200

    up

--- src/camera.c
+++ src/camera.c
@@ -6,11 +6,26 @@
 /*   By: sduprey <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/07 05:31:21 by sduprey           #+#    #+#             */
-/*   Updated: 2016/07/08 02:30:59 by bhenne           ###   ########.fr       */
+/*   Updated: 2016/07/08 02:56:08 by bhenne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rtv1.h" 
+
+void		calc_pos_plan(t_env *e)
+{
+	t_vec		up_vec;
+	t_vec		right_vec;
+	t_vec		res;
+	t_vec		dir;
+
+	mul_vec_val(&right_vec, &e->cam.right, 0.5 / 2.0);
+	mul_vec_val(&up_vec, &e->cam.down, 0.35 / 2.0);
+	mul_vec_val(&dir, &e->cam.dir, 1.0);
+	sub_vec(&res, &up_vec, &right_vec);
+	add_vec(&res, &res, &dir);
+	add_vec(&e->pos_plan, &res, &e->cam.ori);
+}
 
 t_cam	*init_camera(t_vec *pos, t_vec *dir)
 {
@@ -38,17 +53,3 @@ t_vec	*calcPos(t_cam *cam, int x, int y)
 	return(cam->viewPlaneUpLeft + cam->rightVec*xIndent*x - cam->upVec*yIndent*y);
 }
 
-void		compute_pos_plan(t_env *e)
-{
-	t_vec		up_vec;
-	t_vec		right_vec;
-	t_vec		res;
-	t_vec		dir;
-
-	mul_vec_val(&right_vec, &e->cam.right, 0.5 / 2.0);
-	mul_vec_val(&up_vec, &e->cam.down, 0.35 / 2.0);
-	mul_vec_val(&dir, &e->cam.dir, 1.0);
-	sub_vec(&res, &up_vec, &right_vec);
-	add_vec(&res, &res, &dir);
-	add_vec(&e->pos_plan, &res, &e->cam.ori);
-}
