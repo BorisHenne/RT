b1a59c8fbfbba458d8078e3fc480b134159feebf
Sylvain DUPREY <sduprey@e1r12p16.42.fr>
2016-07-14 05:41:46 +0200

    modif cone.c

--- src/cone.c
+++ src/cone.c
@@ -6,11 +6,12 @@
 /*   By: sduprey <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/14 01:24:52 by sduprey           #+#    #+#             */
-/*   Updated: 2016/07/14 03:32:22 by sduprey          ###   ########.fr       */
+/*   Updated: 2016/07/14 05:39:18 by sduprey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cone.h>
+#include <stdio.h>
 
 double		find_cone_det(t_ray ray, t_cone cone, double *a, double*b)
 {
@@ -18,23 +19,68 @@ double		find_cone_det(t_ray ray, t_cone cone, double *a, double*b)
 	t_vec	tmp;
 
 	tmp = vec_sub(cone.pos, ray.pos);
-
-	*a = pow(ray.dir.x, 2) + pow(ray.dir.y, 2) - pow(ray.dir.z, 2);
-	*b = 2.0  * (tmp.x * ray.dir.x + tmp.y * ray.dir.y - tmp.z * ray.dir.z);
-	c = pow(tmp.x, 2) + pow(tmp.y, 2) - pow(tmp.z, 2);
+	*a = pow(ray.dir.x, 2) + pow(ray.dir.z, 2) - pow(ray.dir.y, 2) * pow(tan(cone.ang), 2);
+	*b = 2.0  * (tmp.x * ray.dir.x + tmp.z * ray.dir.z - tmp.y * ray.dir.y * pow(tan(cone.ang), 2));
+	c = pow(tmp.x, 2) + pow(tmp.z, 2) - pow(tmp.y, 2) * pow(tan(cone.ang), 2);
+	
+	
+	/*
+	*a = pow(ray.dir.x, 2) + pow(ray.dir.y, 2) - pow(ray.dir.z, 2) * pow(tan(cone.ang), 2);
+	*b = 2.0  * (tmp.x * ray.dir.x + tmp.y * ray.dir.y - tmp.z * ray.dir.z * pow(tan(cone.ang), 2));
+	c = pow(tmp.x, 2) + pow(tmp.y, 2) - pow(tmp.z, 2) * pow(tan(cone.ang), 2);
+	*/
 	return (pow(*b, 2) - (4 * (*a) * c));
 }
 
-/*
-double		find_cone_closest_hit(double a, double b, double det)
+double		find_cone_heigth(t_ray ray, t_cone cone, double t1, double t2)
 {
-	double t1;
-	double t2;
+	double hit_z;
+	double max_heigth;
+/*
+	hit_z = ray.pos.z + t1 * ray.dir.z;
+	max_heigth = cone.pos.z + cone.len;
+	if (cone.pos.z <= hit_z && hit_z <= max_heigth)
+	{
+		return (t1);
+	}
+	else
+	{
+			hit_z = ray.pos.z + t2 * ray.dir.z;
+			max_heigth = cone.pos.z + cone.len;
+			if (cone.pos.z <= hit_z && hit_z <= max_heigth)
+				return (t2);
+			else
+				return (0.0f);
+	}
 
-	t1 = ()
+	*/
+	hit_z = ray.pos.y + t1 * ray.dir.y;
+	max_heigth = cone.pos.y + cone.len;
+	if (cone.pos.y <= hit_z && hit_z <= max_heigth)
+	{
+		return (t1);
+	}
+	else
+	{
+			hit_z = ray.pos.y + t2 * ray.dir.y;
+			max_heigth = cone.pos.y + cone.len;
+			if (cone.pos.y <= hit_z && hit_z <= max_heigth)
+				return (t2);
+			else
+				return (0.0f);
+	}
 }
-*/
-#include <stdio.h>
+
+double		find_cone_closest_hit(t_ray ray, t_cone cone, double a, double b, double det)
+{
+	double	t1;
+	double	t2;
+
+	t1 = (-b - sqrt(det)) / (2.0f * a);
+	t2 = (-b + sqrt(det)) / (2.0f * a);
+	return (find_cone_heigth(ray, cone, t1, t2));
+}
+
 t_coord		is_cone_hit(t_ray ray, t_cone cone)
 {
 	t_coord	hit;
@@ -61,9 +107,15 @@ t_coord		is_cone_hit(t_ray ray, t_cone cone)
 	}
 	else if (det > 0)
 	{
-		hit.bool = 1;
-		hit.t = find_sphere_closest_hit(a, b, det);
-		hit.color = cone.color;
+		if ((hit.t = find_cone_closest_hit(ray, cone, a, b, det)) != 0.0f)
+		{
+			hit.bool = 1;
+			hit.color = cone.color;
+		}
+		else
+		{
+			hit.bool = 0;
+		}
 		//printf("test %f\n", det);
 	}
 	//else
