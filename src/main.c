3bdb9e3c61a6b44cb482ddd83078f336c3ac6fb4
Thomas LEPECHEUR <tlepeche@e1r8p11.42.fr>
2016-07-20 05:03:12 +0200

    push du main avec couleur objet entre 0 et 1 (c'est la valeur de l'absorbtion, 1 = 255, 0 = 0)

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: sduprey <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/07 00:15:41 by sduprey           #+#    #+#             */
-/*   Updated: 2016/07/17 22:36:21 by nbelouni         ###   ########.fr       */
+/*   Updated: 2016/07/20 04:40:17 by tlepeche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
@@ -23,10 +23,10 @@ t_scene		init_all(void)
 {
 	t_vec ori;
 	ori.x = 0.0;
-	ori.y = 0.0;
+	ori.y = -1.0;
 	ori.z = -10.0;
 	t_vec look;
-	look.x = deg_to_rad(0);
+	look.x = deg_to_rad(-5);
 	look.y = deg_to_rad(0);
 	look.z = deg_to_rad(0);
 	t_cam cam;
@@ -35,25 +35,25 @@ t_scene		init_all(void)
 
 	t_sphere	*sphere2;
 	sphere2 = (t_sphere *)malloc(sizeof(t_sphere));
-	sphere2->radius = 0.5;
-	sphere2->center.x = 0.0;
-	sphere2->center.y = 0.0;
-	sphere2->center.z = 0.0;
-	sphere2->color.r = 0;
-	sphere2->color.g = 223;
-	sphere2->color.b = 223;
+	sphere2->radius = 0.3;
+	sphere2->center.x = -0.35;
+	sphere2->center.y = 0.5;
+	sphere2->center.z = 0;//-0.5;
+	sphere2->color.r = 1;
+	sphere2->color.g = 1;
+	sphere2->color.b = 0.1;
 
 	t_sphere	*sphere3;
 	sphere3 = (t_sphere *)malloc(sizeof(t_sphere));
 	sphere3->radius = 0.3;
-	sphere3->center.x = 0.0;
-	sphere3->center.y = 0.7;
+	sphere3->center.x = 0.35;
+	sphere3->center.y = 0.5;
 	sphere3->center.z = 0.0;
-	sphere3->color.r = 255;
-	sphere3->color.g = 255;
-	sphere3->color.b = 0;
+	sphere3->color.r = 1;
+	sphere3->color.g = 0.1;
+	sphere3->color.b = 0.1;
 
-	t_sphere	*sphere4;
+/*	t_sphere	*sphere4;
 	sphere4 = (t_sphere *)malloc(sizeof(t_sphere));
 	sphere4->radius = 0.2;
 	sphere4->center.x = -0.2;
@@ -79,7 +79,7 @@ t_scene		init_all(void)
 	cone->len = 0.8;
 	cone->color.r = 173;
 	cone->color.g = 0;
-	cone->color.b = 123;
+	cone->color.b = 123;*/
 
 	t_plane	   *ground;
 	ground = (t_plane *)malloc(sizeof(t_plane));
@@ -89,45 +89,45 @@ t_scene		init_all(void)
 	ground->normal.x = 0.0;
 	ground->normal.y = 1.0;
 	ground->normal.z = 0.0;
-	ground->color.r = 255;
-	ground->color.g = 255;
-	ground->color.b = 255;
+	ground->color.r = 1;
+	ground->color.g = 1;
+	ground->color.b = 1;
 
 	t_light		*light;
 	light = (t_light *)malloc(sizeof(t_light));
-	light->pos.x = -5.0;
-	light->pos.y = -5.0;
-	light->pos.z = 0.0;
+	light->pos.x = 2.0;
+	light->pos.y = -2.0;
+	light->pos.z = 2.0;
 	light->color.r = 255;
 	light->color.g = 255;
 	light->color.b = 255;
 
 	t_light		*light2;
 	light2 = (t_light *)malloc(sizeof(t_light));
-	light2->pos.x = 5.0;
-	light2->pos.y = -5.0;
-	light2->pos.z = 0.0;
+	light2->pos.x = -2.0;
+	light2->pos.y = -2.;
+	light2->pos.z = 2.0;
 	light2->color.r = 255;
 	light2->color.g = 255;
 	light2->color.b = 255;
 
 	t_light		*light3;
 	light3 = (t_light *)malloc(sizeof(t_light));
-	light3->pos.x = 0.0;
-	light3->pos.y = 0.0;
-	light3->pos.z = -5.0;
-	light3->color.r = 255;
-	light3->color.g = 255;
-	light3->color.b = 255;
+	light3->pos.x = -2.0;
+	light3->pos.y = -2.0;
+	light3->pos.z = -2.0;
+	light3->color.r = 100;
+	light3->color.g = 100;
+	light3->color.b = 100;
 
 	t_light		*light4;
 	light4 = (t_light *)malloc(sizeof(t_light));
-	light4->pos.x = 0.0;
-	light4->pos.y = -5.0;
-	light4->pos.z = 0.0;
-	light4->color.r = 255;
-	light4->color.g = 255;
-	light4->color.b = 255;
+	light4->pos.x = 2.0;
+	light4->pos.y = -2.0;
+	light4->pos.z = -2.0;
+	light4->color.r = 100;
+	light4->color.g = 100;
+	light4->color.b = 100;
 
 
 	t_node		*node;
@@ -140,23 +140,23 @@ t_scene		init_all(void)
 	node_add(&(scene.objects), node);
 	node = init_node(SPHERE, sphere3, "sphere 2 couleurs", 2);
 	node_add(&(scene.objects), node);
-	node = init_node(SPHERE, sphere4, "sphere 3 couleurs", 3);
+/*	node = init_node(SPHERE, sphere4, "sphere 3 couleurs", 3);
 	node_add(&(scene.objects), node);
 	node = init_node(CYLINDER, cylinder, "cylinder", 4);
 	node_add(&(scene.objects), node);
 	node = init_node(CONE, cone, "cone", 5);
-	node_add(&(scene.objects), node);
+	node_add(&(scene.objects), node);*/
 	node = init_node(PLANE, ground, "ground", 6);
 	node_add(&(scene.objects), node);
 
 	node = init_node(LIGHT, light, "light 1", 1);
-	node_add(&(scene.lights), node);
-	node = init_node(LIGHT, light2, "light 2", 2);
-	node_add(&(scene.lights), node);
+//	node_add(&(scene.lights), node);
+//	node = init_node(LIGHT, light2, "light 2", 2);
+//	node_add(&(scene.lights), node);
 	node = init_node(LIGHT, light3, "light 3", 3);
 	node_add(&(scene.lights), node);
-	node = init_node(LIGHT, light4, "light 4", 4);
-	node_add(&(scene.lights), node);
+//	node = init_node(LIGHT, light4, "light 4", 4);
+//	node_add(&(scene.lights), node);
 
 	return (scene);
 }
