c73be71e390fbf3df2e82c64edbeb2091c12b685
Thomas LEPECHEUR <tlepeche@e1r2p19.42.fr>
2016-08-05 04:36:53 +0200

    oubli .h

--- inc/refraction.h
+++ inc/refraction.h
@@ -6,14 +6,15 @@
 /*   By: nbelouni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/22 04:59:45 by nbelouni          #+#    #+#             */
-/*   Updated: 2016/07/25 16:49:17 by nbelouni         ###   ########.fr       */
+/*   Updated: 2016/08/04 04:26:24 by tlepeche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef REFRACTION_H
 # define REFRACTION_H
 
-t_ray	find_refract_vect(t_ray start_ray, t_hit drawn_pixel, double c_r, double next_r, int is_in_object);
-t_color	apply_refraction(t_ray start, t_scene scene, t_hit drawn_pixe, double reflet);
+//t_ray	find_refract_vect(t_ray start_ray, t_hit drawn_pixel, double c_r, double next_r, int is_in_object);
+//t_color	apply_refraction(t_ray start, t_scene scene, t_hit drawn_pixe, double reflet);
+t_color	apply_refraction(t_ray start, t_scene scene, t_hit drawn_pixe, double reflet, double noise);
 
 #endif
