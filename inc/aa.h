6d6ef06eca4aff8926ccd00cee0f50d32d8d4d3a
Sylvain DUPREY <sduprey@e1r2p20.42.fr>
2016-08-07 04:42:18 +0200

    anti aliasing + filtre sepia + N&B

--- inc/aa.h
+++ inc/aa.h
@@ -6,7 +6,7 @@
 /*   By: sduprey <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/04 00:11:00 by sduprey           #+#    #+#             */
-/*   Updated: 2016/08/04 06:13:08 by sduprey          ###   ########.fr       */
+/*   Updated: 2016/08/05 00:30:03 by sduprey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,7 @@
 
 void	*aa(void *mlx, void *img, int mode);
 t_color	get_pixel_color(void *img, int x, int y);
+void	*sepia_filter(void *, void *);
+int		put_pixel_on_image(void *img, int x, int y, t_color color);
 
 # endif
